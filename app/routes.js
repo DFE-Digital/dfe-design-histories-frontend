d103aff396e8e403584931c92d2f85dee480a0d1
Andy Jones <andy.jones@digital.education.gov.uk>
2022-10-23 13:46:10 +0100

    Added breadcrumbs

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,89 @@
 const express = require('express')
 const router = express.Router()
 const fetch = require('node-fetch')
+const  axios = require('axios')
 
 // Add your routes here - above the module.exports line
 
 
+router.get('/accessibility-statement', function (req, res) {
+    
+console.log('accessibility-statement')
+
+    var config = {
+        method: 'get',
+        url: `${process.env.cmsurl}api/service-pages/1`,
+        headers: {
+          Authorization: 'Bearer ' + process.env.apikey,
+        },
+      }
+    
+      axios(config)
+        .then(function (response) {
+          var data = response.data
+
+            console.log(data.error)
+    
+          res.render('generic-page',{data})
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+  
+})
+
+router.get('/get-a-design-history', function (req, res) {
+    
+    console.log('accessibility-statement')
+    
+        var config = {
+            method: 'get',
+            url: `${process.env.cmsurl}api/service-pages/2`,
+            headers: {
+              Authorization: 'Bearer ' + process.env.apikey,
+            },
+          }
+        
+          axios(config)
+            .then(function (response) {
+              var data = response.data
+    
+                console.log(data.error)
+        
+              res.render('generic-page',{data})
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
+      
+    })
+
+
+    router.get('/team/:id', function (req, res) {
+
+        var config = {
+          method: 'get',
+          url: `${process.env.cmsurl}api/teams?filters[slug][\$eq]=${req.params.id}&populate=%2A`,
+          headers: {
+            Authorization: 'Bearer ' + process.env.apikey,
+          },
+        }
+    
+      axios(config)
+        .then(function (response) {
+          var services = response.data
+      
+            res.render('services', { services })
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+          
+      })
+
+
   router.get('/', function (req, res) {
-    var axios = require('axios')
+   
   
     var config = {
       method: 'get',
@@ -53,8 +130,7 @@ const fetch = require('node-fetch')
 
 
 router.get('/:service_slug/:post_slug', function (req, res) {
-  var axios = require('axios')
-
+  
   var config = {
     method: 'get',
     url: `${process.env.cmsurl}api/posts?filters[slug][\$eq]=${req.params.post_slug}&[service][slug][\$eq]=${req.params.service_slug}&populate=%2A`,
@@ -63,20 +139,36 @@ router.get('/:service_slug/:post_slug', function (req, res) {
     },
   }
 
-  axios(config)
-    .then(function (response) {
-      var data = response.data
+  var service = {
+    method: 'get',
+    url: `${process.env.cmsurl}api/services?filters[slug][\$eq]=${req.params.service_slug}&populate=%2A`,
+    headers: {
+      Authorization: 'Bearer ' + process.env.apikey,
+    },
+  }
 
-      res.render('post', { data })
+  axios(config)
+  .then(function (response) {
+    var data = response.data
+
+    axios(service)
+    .then(function (responses) {
+      var services = responses.data
+
+      res.render('post', { data, services })
     })
     .catch(function (error) {
       console.log(error)
     })
+    
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
 })
 
 
 router.get('/:id', function (req, res) {
-  var axios = require('axios')
 
   var config = {
     method: 'get',
